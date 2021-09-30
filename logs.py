2b7199e9d7b86bea361752a0deee0ef571244176
Remigius <37455393+RemGo95@users.noreply.github.com>
2021-09-30 10:07:21 +0200

    Update logs.py
    There is still a problem with line self.writer = filewriter(self.log_dir)

--- logs.py
+++ logs.py
@@ -1,5 +1,7 @@
 from keras.callbacks import TensorBoard
-from tensorflow.summary import FileWriter
+#from tensorflow.summary import FileWriter
+import tensorflow as tf
+from keras.models import filewriter
 
 class CustomTensorBoard(TensorBoard):
     def __init__(self, **kwargs):
@@ -9,5 +11,7 @@ class CustomTensorBoard(TensorBoard):
     def set_model(self, model):
         pass
 
+    #Problematic function in new version of libs
     def log(self, step, **stats):
-        self._write_logs(stats, step)
+       # self._write_logs(stats, step)
+        self.writer = filewriter(self.log_dir)  
